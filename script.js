de29bc8187ed0c39889e87f437b22d6e26f9783c
hermkan <hermann.kanga.dev@gmail.com>
2021-12-06 22:28:53 -0500

    completed feedback

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-const toggle = document.getElementById('toggle');
-const nav = document.getElementById('nav');
-
-toggle.addEventListener('click', () => nav.classList.toggle('active'));
+const ratingContainer = document.querySelector('.ratings-container');
+const ratings = document.querySelectorAll('.rating');
+const sendBtn = document.querySelector('#send');
+const panel = document.querySelector('#panel');
+let selectedRating = 'Satisfied';
+ratingContainer.addEventListener('click', (e) => {
+  if (e.target.parentNode.classList.contains('rating')) {
+    removeActive();
+    e.target.parentNode.classList.add('active');
+    selectedRating = e.target.nextElementSibling.innerHTML;
+  }
+});
+sendBtn.addEventListener('click', (e) => {
+  panel.innerHTML = ` <i class="fas fa-heart"></i>
+  <strong>Thank you for your<strong/>
+  <br>
+  <strong>Feedback<strong/>
+  <p>We'll use your feedback to imporove our customer support<p/>
+  `;
+});
+function removeActive() {
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove('active');
+  }
+}
